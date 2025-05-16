3c702fb6c9618f5a261768d934742f73d5f57941
Luca <luca.saggese@gmail.com>
2025-05-16 20:36:04 +0200

    wip

--- app/sana_pipeline_i2i.py
+++ app/sana_pipeline_i2i.py
@@ -164,145 +164,145 @@ class SanaPipeline(nn.Module):
     def register_progress_bar(self, progress_fn=None):
         self.progress_fn = progress_fn if progress_fn is not None else self.progress_fn
 
-   @torch.inference_mode()
-def forward(
-    self,
-    prompt=None,
-    height=1024,
-    width=1024,
-    input_image: Optional[torch.Tensor] = None,
-    strength: float = 0.5,
-    negative_prompt="",
-    num_inference_steps=20,
-    guidance_scale=4.5,
-    pag_guidance_scale=1.0,
-    num_images_per_prompt=1,
-    generator=torch.Generator().manual_seed(42),
-    latents=None,
-    use_resolution_binning=True,
-):
-    self.ori_height, self.ori_width = height, width
-    if use_resolution_binning:
-        self.height, self.width = classify_height_width_bin(height, width, ratios=self.base_ratios)
-    else:
-        self.height, self.width = height, width
-    self.latent_size_h, self.latent_size_w = (
-        self.height // self.config.vae.vae_downsample_rate,
-        self.width // self.config.vae.vae_downsample_rate,
-    )
-    self.guidance_type = guidance_type_select(self.guidance_type, pag_guidance_scale, self.config.model.attn_type)
-
-    if negative_prompt != "":
-        null_caption_token = self.tokenizer(
-            negative_prompt,
-            max_length=self.max_sequence_length,
-            padding="max_length",
-            truncation=True,
-            return_tensors="pt",
-        ).to(self.device)
-        self.null_caption_embs = self.text_encoder(null_caption_token.input_ids, null_caption_token.attention_mask)[0]
-
-    if prompt is None:
-        prompt = [""]
-    prompts = prompt if isinstance(prompt, list) else [prompt]
-    samples = []
-
-    for prompt in prompts:
-        prompts, hw, ar = (
-            [],
-            torch.tensor([[self.image_size, self.image_size]], dtype=torch.float, device=self.device).repeat(
-                num_images_per_prompt, 1
-            ),
-            torch.tensor([[1.0]], device=self.device).repeat(num_images_per_prompt, 1),
+    @torch.inference_mode()
+    def forward(
+        self,
+        prompt=None,
+        height=1024,
+        width=1024,
+        input_image: Optional[torch.Tensor] = None,
+        strength: float = 0.5,
+        negative_prompt="",
+        num_inference_steps=20,
+        guidance_scale=4.5,
+        pag_guidance_scale=1.0,
+        num_images_per_prompt=1,
+        generator=torch.Generator().manual_seed(42),
+        latents=None,
+        use_resolution_binning=True,
+    ):
+        self.ori_height, self.ori_width = height, width
+        if use_resolution_binning:
+            self.height, self.width = classify_height_width_bin(height, width, ratios=self.base_ratios)
+        else:
+            self.height, self.width = height, width
+        self.latent_size_h, self.latent_size_w = (
+            self.height // self.config.vae.vae_downsample_rate,
+            self.width // self.config.vae.vae_downsample_rate,
         )
+        self.guidance_type = guidance_type_select(self.guidance_type, pag_guidance_scale, self.config.model.attn_type)
 
-        for _ in range(num_images_per_prompt):
-            prompts.append(prepare_prompt_ar(prompt, self.base_ratios, device=self.device, show=False)[0].strip())
-
-        with torch.no_grad():
-            if not self.config.text_encoder.chi_prompt:
-                max_length_all = self.config.text_encoder.model_max_length
-                prompts_all = prompts
-            else:
-                chi_prompt = "\n".join(self.config.text_encoder.chi_prompt)
-                prompts_all = [chi_prompt + prompt for prompt in prompts]
-                num_chi_prompt_tokens = len(self.tokenizer.encode(chi_prompt))
-                max_length_all = (
-                    num_chi_prompt_tokens + self.config.text_encoder.model_max_length - 2
-                )
-
-            caption_token = self.tokenizer(
-                prompts_all,
-                max_length=max_length_all,
+        if negative_prompt != "":
+            null_caption_token = self.tokenizer(
+                negative_prompt,
+                max_length=self.max_sequence_length,
                 padding="max_length",
                 truncation=True,
                 return_tensors="pt",
-            ).to(device=self.device)
-            select_index = [0] + list(range(-self.config.text_encoder.model_max_length + 1, 0))
-            caption_embs = self.text_encoder(caption_token.input_ids, caption_token.attention_mask)[0][:, None][
-                :, :, select_index
-            ].to(self.weight_dtype)
-            emb_masks = caption_token.attention_mask[:, select_index]
-            null_y = self.null_caption_embs.repeat(len(prompts), 1, 1)[:, None].to(self.weight_dtype)
+            ).to(self.device)
+            self.null_caption_embs = self.text_encoder(null_caption_token.input_ids, null_caption_token.attention_mask)[0]
 
-            model_kwargs = dict(data_info={"img_hw": hw, "aspect_ratio": ar}, mask=emb_masks)
+        if prompt is None:
+            prompt = [""]
+        prompts = prompt if isinstance(prompt, list) else [prompt]
+        samples = []
 
-            if self.vis_sampler == "flow_dpm-solver":
-                scheduler = DPMS(
-                    self.model,
-                    condition=caption_embs,
-                    uncondition=null_y,
-                    guidance_type=self.guidance_type,
-                    cfg_scale=guidance_scale,
-                    pag_scale=pag_guidance_scale,
-                    pag_applied_layers=self.config.model.pag_applied_layers,
-                    model_type="flow",
-                    model_kwargs=model_kwargs,
-                    schedule="FLOW",
-                )
-                scheduler.register_progress_bar(self.progress_fn)
+        for prompt in prompts:
+            prompts, hw, ar = (
+                [],
+                torch.tensor([[self.image_size, self.image_size]], dtype=torch.float, device=self.device).repeat(
+                    num_images_per_prompt, 1
+                ),
+                torch.tensor([[1.0]], device=self.device).repeat(num_images_per_prompt, 1),
+            )
 
-                if input_image is not None:
-                    input_image = input_image.to(self.device).to(self.vae_dtype)
-                    z_0 = self.vae.encode(input_image).latent_dist.sample()
-                    z_0 = z_0.to(self.weight_dtype)
+            for _ in range(num_images_per_prompt):
+                prompts.append(prepare_prompt_ar(prompt, self.base_ratios, device=self.device, show=False)[0].strip())
 
-                    t_index = int(strength * num_inference_steps)
-                    noise = torch.randn_like(z_0)
-                    t = torch.tensor([t_index], device=self.device, dtype=torch.long)
-                    z = scheduler.add_noise(z_0, noise, t)
-                elif latents is not None:
-                    z = latents.to(self.device)
+            with torch.no_grad():
+                if not self.config.text_encoder.chi_prompt:
+                    max_length_all = self.config.text_encoder.model_max_length
+                    prompts_all = prompts
                 else:
-                    z = torch.randn(
-                        len(prompts),
-                        self.config.vae.vae_latent_dim,
-                        self.latent_size_h,
-                        self.latent_size_w,
-                        generator=generator,
-                        device=self.device,
+                    chi_prompt = "\n".join(self.config.text_encoder.chi_prompt)
+                    prompts_all = [chi_prompt + prompt for prompt in prompts]
+                    num_chi_prompt_tokens = len(self.tokenizer.encode(chi_prompt))
+                    max_length_all = (
+                        num_chi_prompt_tokens + self.config.text_encoder.model_max_length - 2
                     )
 
-                sample = scheduler.sample(
-                    z,
-                    steps=num_inference_steps,
-                    order=2,
-                    skip_type="time_uniform_flow",
-                    method="multistep",
-                    flow_shift=self.flow_shift,
-                )
-            else:
-                raise NotImplementedError("Image-to-image attualmente supportato solo per flow_dpm-solver")
+                caption_token = self.tokenizer(
+                    prompts_all,
+                    max_length=max_length_all,
+                    padding="max_length",
+                    truncation=True,
+                    return_tensors="pt",
+                ).to(device=self.device)
+                select_index = [0] + list(range(-self.config.text_encoder.model_max_length + 1, 0))
+                caption_embs = self.text_encoder(caption_token.input_ids, caption_token.attention_mask)[0][:, None][
+                    :, :, select_index
+                ].to(self.weight_dtype)
+                emb_masks = caption_token.attention_mask[:, select_index]
+                null_y = self.null_caption_embs.repeat(len(prompts), 1, 1)[:, None].to(self.weight_dtype)
 
-        sample = sample.to(self.vae_dtype)
-        with torch.no_grad():
-            sample = vae_decode(self.config.vae.vae_type, self.vae, sample)
+                model_kwargs = dict(data_info={"img_hw": hw, "aspect_ratio": ar}, mask=emb_masks)
 
-        if use_resolution_binning:
-            sample = resize_and_crop_tensor(sample, self.ori_width, self.ori_height)
-        samples.append(sample)
+                if self.vis_sampler == "flow_dpm-solver":
+                    scheduler = DPMS(
+                        self.model,
+                        condition=caption_embs,
+                        uncondition=null_y,
+                        guidance_type=self.guidance_type,
+                        cfg_scale=guidance_scale,
+                        pag_scale=pag_guidance_scale,
+                        pag_applied_layers=self.config.model.pag_applied_layers,
+                        model_type="flow",
+                        model_kwargs=model_kwargs,
+                        schedule="FLOW",
+                    )
+                    scheduler.register_progress_bar(self.progress_fn)
 
-        return sample
+                    if input_image is not None:
+                        input_image = input_image.to(self.device).to(self.vae_dtype)
+                        z_0 = self.vae.encode(input_image).latent_dist.sample()
+                        z_0 = z_0.to(self.weight_dtype)
 
-    return samples
+                        t_index = int(strength * num_inference_steps)
+                        noise = torch.randn_like(z_0)
+                        t = torch.tensor([t_index], device=self.device, dtype=torch.long)
+                        z = scheduler.add_noise(z_0, noise, t)
+                    elif latents is not None:
+                        z = latents.to(self.device)
+                    else:
+                        z = torch.randn(
+                            len(prompts),
+                            self.config.vae.vae_latent_dim,
+                            self.latent_size_h,
+                            self.latent_size_w,
+                            generator=generator,
+                            device=self.device,
+                        )
+
+                    sample = scheduler.sample(
+                        z,
+                        steps=num_inference_steps,
+                        order=2,
+                        skip_type="time_uniform_flow",
+                        method="multistep",
+                        flow_shift=self.flow_shift,
+                    )
+                else:
+                    raise NotImplementedError("Image-to-image attualmente supportato solo per flow_dpm-solver")
+
+            sample = sample.to(self.vae_dtype)
+            with torch.no_grad():
+                sample = vae_decode(self.config.vae.vae_type, self.vae, sample)
+
+            if use_resolution_binning:
+                sample = resize_and_crop_tensor(sample, self.ori_width, self.ori_height)
+            samples.append(sample)
+
+            return sample
+
+        return samples
 
